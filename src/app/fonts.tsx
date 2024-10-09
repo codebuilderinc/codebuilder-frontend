037d02e74ba132dc7c7de722569da9fe01812efd
digitalnomad91 <andrew.c.corbin@gmail.com>
2024-10-09 01:33:45 -0500

    Added fonts component.

---
+++ src/app/fonts.tsx
@@ -0,0 +1,38 @@
+import { Pacifico as PacificoFont } from 'next/font/google'
+import localFont from 'next/font/local'
+import { Roboto as RobotoFont } from 'next/font/google'
+import { Raleway as RalewayFont } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
+
+export const OpenSans = Open_Sans({
+  subsets: ['latin'], // Add other subsets as needed
+  weight: ['400', '700'], // Define weights (optional)
+})
+
+// Load the font at the module level
+export const Raleway = RalewayFont({
+  subsets: ['latin'], // Add other subsets as needed
+  weight: ['400', '700'], // Define weights (optional)
+})
+
+// Load the font at the module level
+export const Roboto = RobotoFont({
+  subsets: ['latin'], // You can add more subsets like 'latin-ext'
+  weight: ['400', '700'], // Define weights (optional)
+})
+
+export const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+})
+
+export const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+})
+export const Pacifico = PacificoFont({
+  subsets: ['latin'], // You can include 'latin' or other subsets
+  weight: '400', // Specify the font weight (optional)
+})
