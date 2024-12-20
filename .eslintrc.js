c9ef17d7e82b118e1120c250fc4b504ec7aaf2ef
digitalnomad91 <andrew.c.corbin@gmail.com>
2024-12-19 21:40:22 -0600

    Updated extends prop.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: ['next/core-web-vitals', 'next/typescript'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
