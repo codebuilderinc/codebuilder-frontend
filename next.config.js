2921924ca809a8f282b502a89a71f6a15f914b3a
digitalnomad91 <andrew.c.corbin@gmail.com>
2024-12-19 21:35:34 -0600

    Removed dynamically generated props in github workflow.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'export',
-  basePath: '/codebuilder-frontend', // Replace with your GitHub repository name
   trailingSlash: true, // Ensures compatibility with GitHub Pages
 }
