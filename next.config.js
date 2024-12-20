65ed883687d1a42a296c734ef5dd1bc8e12e6123
digitalnomad91 <andrew.c.corbin@gmail.com>
2024-12-19 21:31:07 -0600

    Config for github pages.

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  basePath: '/codebuilder-frontend', // Replace with your GitHub repository name
+  trailingSlash: true, // Ensures compatibility with GitHub Pages
 }
