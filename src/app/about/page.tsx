f7d9346ccafc0ab1f5f2c535a0f2f9748053c240
digitalnomad91 <andrew.c.corbin@gmail.com>
2024-12-19 19:16:48 -0600

    Added video player component to about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image'
-import VideoPlayer from '../../Components/VideoPlayer'
+import VideoPlayer from '../../components/VideoPlayer'
 
 export default function About() {
   return (
-    <div className="flex min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="font-[family-name:var(--font-geist-sans)]">
       {/* Hero Slider */}
-      <div className="absolute top-[-75px] left-0 w-full h-[500px] overflow-hidden opacity-40">
-        <VideoPlayer
-          mp4Src="/videos/macbook-typing-about-us.mp4"
-          webmSrc="/videos/macbook-typing-about-us.webm"
-          posterSrc="/videos/cover-images/macbook-typing-poster.jpg"
-        />
+      <div className="relative top-[-75px] w-screen h-[375px] overflow-hidden bg-black">
+        <div className="opacity-40">
+          <VideoPlayer
+            mp4Src="/videos/macbook-typing-about-us.mp4"
+            webmSrc="/videos/macbook-typing-about-us.webm"
+            posterSrc="/videos/cover-images/macbook-typing-poster.jpg"
+          />
+        </div>
       </div>
 
       {/* Main Content */}
-      <div className="relative z-10 flex flex-col items-center sm:items-start pt-[500px]">
-        <div className="flex w-screen h-screen">
+      <div className="z-10 flex items-center sm:items-start ">
+        <div className="flex w-screen">
           <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
             <li className="mb-2">
               Get started by editing{' '}
@@ -27,7 +29,7 @@ export default function About() {
             <li>Save and see your changes instantly.</li>
           </ol>
 
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <div className="flex items-center sm:flex-row">
             <a
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
               href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -54,55 +56,6 @@ export default function About() {
           </div>
         </div>
       </div>
-
-      {/* Footer */}
-      <footer className="flex gap-6 flex-wrap items-center justify-center mt-8">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   )
 }
