544f4716a10cb4cb624023e0a2c7b47079df2a75
digitalnomad91 <andrew.c.corbin@gmail.com>
2024-12-19 19:04:23 -0600

    Added utils folder and reddit api library.

---
+++ src/lib/reddit.ts
@@ -0,0 +1,21 @@
+// lib/reddit.ts
+
+import axios from 'axios'
+
+export const fetchRedditPosts = async (subreddits: string[]) => {
+  const posts = []
+
+  for (const subreddit of subreddits) {
+    const response = await axios.get(`https://www.reddit.com/r/${subreddit}/new.json?limit=10`)
+    const subredditPosts = response.data.data.children.map((child: any) => ({
+      title: child.data.title,
+      author: child.data.author,
+      subreddit: child.data.subreddit,
+      url: child.data.url,
+      createdAt: new Date(child.data.created_utc * 1000),
+    }))
+    posts.push(...subredditPosts)
+  }
+
+  return posts
+}
