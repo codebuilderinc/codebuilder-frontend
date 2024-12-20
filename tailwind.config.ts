687eb921d907e8508cd0026d8077f18344664dce
digitalnomad91 <andrew.c.corbin@gmail.com>
2024-12-19 19:18:12 -0600

    Added custom colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,53 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/app/**/*.{js,ts,jsx,tsx}', // Include files in src/app
+    './src/components/**/*.{js,ts,jsx,tsx}', // Optionally include other directories
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        cbBlue: {
+          1: '#7efde3',
+          2: '#29d3fe',
+          3: '#0087a9',
+          4: '#08afde',
+          5: '#26b7e1',
+          6: '#09afdf',
+        },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        grey: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        green: colors.emerald,
+        purple: colors.violet,
+        pink: colors.fuchsia,
+        teal: colors.teal,
+        red: colors.red,
+        blue: colors.blue,
+        cyan: colors.cyan,
+        orange: colors.orange,
+        lime: colors.lime,
+        amber: colors.amber,
+        rose: colors.rose,
+        stone: colors.stone,
+        neutral: colors.neutral,
+        slate: colors.slate,
+        zinc: colors.zinc,
+        sky: colors.sky,
+        fuchsia: colors.fuchsia,
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
