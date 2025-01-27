90bb430ab701355615ed47faeb5ff2dd7c183afc
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-01-26 18:10:46 -0600

    Moved next.config to ts.

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
+    },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  trailingSlash: true, // Ensures compatibility with GitHub Pages
+  webpack: (config, { isServer }) => {
+    // Modify Webpack's watchOptions
+    config.watchOptions = {
+      poll: 1000, // Enable polling
+      aggregateTimeout: 300, // Delay before rebuilding
+      ignored: /node_modules/, // Example: Exclude node_modules
+    }
+
+    return config
+  },
+}
+
+export default nextConfig
