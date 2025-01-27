050f9c56fb27b1166ebe5b1092183ef4207bbe65
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-01-26 18:10:35 -0600

    Moved service worker file.

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+// public/service-worker.js
+
+self.addEventListener('push', (event) => {
+  if (event.data) {
+    const data = event.data.json()
+    const options = {
+      body: data.body,
+      icon: data.icon || 'https://new.codebuilder.org/images/logo2.png', // Use a default icon if none is specified
+      badge: data.badge || 'https://new.codebuilder.org/images/logo2.png',
+      data: { url: data.url },
+    }
+
+    event.waitUntil(self.registration.showNotificationew(data.title, options))
+  }
+})
+
+// Handle notification clicks
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close()
+  event.waitUntil(clients.openWindow(event.notification.data.url))
+})
