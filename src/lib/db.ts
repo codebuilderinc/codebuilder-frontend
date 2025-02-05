804291a23fe819e46da1eaae5900bab933e4b1b0
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-02-04 22:39:13 -0600

    Added comments.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,23 @@
+// lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
+// Create a singleton instance of PrismaClient extended with accelerate and logging.
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate())
+  return new PrismaClient({
+    log: ['query'],
+  }).$extends(withAccelerate())
 }
 
+// Use a global variable to persist the Prisma Client across hot reloads in development.
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>
 } & typeof global
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma
+}
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma
